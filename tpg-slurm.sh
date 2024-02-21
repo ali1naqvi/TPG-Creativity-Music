8e6001438ff5ed1af0f2b66e024d38d243ca1ff4
Wasay Naqvi <alinaqvi8014@gmail.com>
2024-02-21 14:06:12 -0500

    research-narval

--- tpg-slurm.sh
+++ tpg-slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH --account=def-skelly
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=512M
-#SBATCH --time=0-36:00  # time (DD-HH:MM)
+#SBATCH --ntasks-per-node=64
+#SBATCH --mem=2G
+#SBATCH --time=0-48:00  # time (DD-HH:MM)
 #SBATCH --error=error_file.txt
 
 seed=$1
 
 module load python/3.10
 
-python /models/generator_TPG.PY -s $seed  --num_proc 32
+python /models/generator_TPG.PY -s $seed  --num_proc 64
