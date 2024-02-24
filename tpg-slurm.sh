634c7f52e8f8a57b178c6c22b4096fc2cc5e5fd2
Wasay Naqvi <alinaqvi8014@gmail.com>
2024-02-24 14:34:22 -0500

    changes with slurm

--- tpg-slurm.sh
+++ tpg-slurm.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --mem=2G
-#SBATCH --time=0-48:00  # time (DD-HH:MM)
+#SBATCH --time=3-10:00  # time (DD-HH:MM)
 #SBATCH --error=error_file.txt
 
 seed=$1
 
 module load python/3.10
 
-python models/generator_TPG.PY -s $seed  --num_proc 64
+python generator_TPG.PY -s $seed  --num_proc 64
